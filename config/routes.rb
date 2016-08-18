191fd75714be3d6c351109ae8a985cb4f1ee5b65
Tomasz Cieńciała <tomasz.cienciala@gmail.com>
2016-08-18 17:19:18 +0200

    39 failures - fixed routes and user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
+  root 'students#index'
   devise_for :users
   resources :students do
     get :subjects
   end
+  resources :teachers
+  resources :report_subjects
 end
