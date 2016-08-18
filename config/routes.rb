7bfb5b5b749a595a9a4b7fe33e271532ff49c1ad
Tomasz Cieńciała <tomasz.cienciala@gmail.com>
2016-08-18 23:04:15 +0200

    13 failures - fixed report_subject error

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
 
   root 'students#index'
+
   devise_for :users
+
   resources :students do
     get :subjects
   end
+
   resources :teachers do
     get :subjects
   end
   resources :report_subjects
+
+  get 'reports', to: 'reports#subjects'
 end
