c5db500c829348a508408a8677a89f9f3ee6d220
Tomasz Cieńciała <tomasz.cienciala@gmail.com>
2016-08-20 14:02:57 +0200

    9 failures - fixed student decorator (avg_notes method)

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,8 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    values = []
+    subject_item.subject_item_notes.each { |note| values << note.value}
+    sprintf('%.2f', values.sum.to_f / subject_item_notes.count)
   end
 end
